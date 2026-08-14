4c4f77ec77ca0b443b382cf3b807291289392b6f
ZhongRuiXiang <rashaunzh@gmail.com>
2026-08-14 19:02:51 +0800

    fix: restore use client directive in product story; support DATABASE_ID env for prod D1 binding

--- app/product/ProductStory.tsx
+++ app/product/ProductStory.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useEffect, useMemo, useState } from "react";
 import { STAGES, ROUTE_NODES, STATUS_TEXT } from "./ProductStory.data";
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import hostingConfig from "./.openai/hosting.json";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
+  process.env.DATABASE_ID ??
   "00000000-0000-4000-8000-000000000000";
 
 const { d1, r2 } = hostingConfig;
